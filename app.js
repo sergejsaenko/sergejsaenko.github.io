f67cbdbd8c6044f50be675c69e332fff0cfde538
sergejsaenko <ssaenko@live.de>
2023-08-02 13:20:20 +0200

    test

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
       }
     });
 
@@ -20,5 +22,16 @@ const navSlide = () => {
     burger.classList.toggle("toggle");
   });
 };
+
+// JavaScript, um die .html Erweiterung aus den Links zu entfernen
+window.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-links a");
+  links.forEach((link) => {
+    const href = link.getAttribute("href");
+    if (href && href.endsWith(".html")) {
+      link.setAttribute("href", href.slice(0, -5)); // Entfernt die letzten 5 Zeichen (.html)
+    }
+  });
+});
 
 navSlide();
