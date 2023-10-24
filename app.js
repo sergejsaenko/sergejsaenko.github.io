d20ce03f28d61097f96b137e4010a8229bc8ba84
sergejsaenko <ssaenko@live.de>
2023-10-24 15:53:59 +0200

    fixed burger menu + resize menu bug

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  const deactivateNav = () => {
+    nav.classList.remove("nav-active");
+    nav.classList.remove("glass_effect");
+    navLinks.forEach((link) => {
+      link.style.animation = "";
+    });
+    burger.classList.remove("toggle");
+  };
 
   burger.addEventListener("click", () => {
     //Toggle Nav
     nav.classList.toggle("nav-active");
+    nav.classList.toggle("glass_effect");
 
     //Animate Links
     navLinks.forEach((link, index) => {
@@ -20,6 +29,14 @@ const navSlide = () => {
 
     //Burger Animation
     burger.classList.toggle("toggle");
+  });
+
+  // Listen for the window resize event
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+      // Deactivate the navigation when the screen is wider than 768px
+      deactivateNav();
+    }
   });
 };
 
