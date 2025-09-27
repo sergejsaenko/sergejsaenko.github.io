c816c1633c28ce642de7cc91d60c8fa8748ecbca
Sergej <ssaenko@live.de>
2025-09-27 16:50:22 +0200

    Init + background video and basic routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Header],
+  template: `
+    <app-header></app-header>
+    <main>
+      <router-outlet></router-outlet>
+    </main>`,
+  styles: `
+    main {
+      padding: 16px;
+      }`,
 })
 export class App {
   protected readonly title = signal('scaffiz_website');
