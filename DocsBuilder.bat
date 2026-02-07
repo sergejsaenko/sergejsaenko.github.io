62c83c521342020eccb2b86e3b1a3fd86f411d93
Sergej <ssaenko@live.de>
2026-02-07 17:23:38 +0100

    hoffentlich build korrigiert.

--- DocsBuilder.bat
+++ DocsBuilder.bat
@@ -2,15 +2,11 @@
 REM Angular Build mit base-href=./
 ng build --configuration production --base-href ./
 
-REM browser-Inhalt nach docs verschieben und browser-Ordner löschen
-powershell -Command "Move-Item -Path .\docs\browser\* -Destination .\docs -Force"
-powershell -Command "Remove-Item -Path .\docs\browser -Recurse -Force"
-
 REM CNAME-Datei mit Domain anlegen
 echo www.scaffiz.de > .\docs\CNAME
 
 REM index.html nach 404.html kopieren
-Copy-Item -Path .\docs\index.html -Destination .\docs\404.html -Force
+powershell -Command "Copy-Item -Path .\docs\index.html -Destination .\docs\404.html -Force"
 
 echo Fertig!
 pause
